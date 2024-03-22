f6c108d5fcc9d851164a6d4f5bfb996fa01fd177
Muhammad Umer Farooq <umer@lablnet.com>
2024-03-22 12:08:37 +0500

    Change tabular view to list view
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- lib/pages/Monthly.dart
+++ lib/pages/Monthly.dart
@@ -8,16 +8,17 @@ import '../utils.dart';
 
 
 class Monthly extends StatefulWidget {
-  double? latitude;
-  double? longitude;
-  int? method;
-  Monthly({this.latitude, this.longitude, this.method});
+  final double? latitude;
+  final double? longitude;
+  final int? method;
+
+  const Monthly({Key? key, this.latitude, this.longitude, this.method}) : super(key: key);
+
   @override
   _MonthlyState createState() => _MonthlyState();
 }
 
-class _MonthlyState extends State<Monthly>
-    with SingleTickerProviderStateMixin {
+class _MonthlyState extends State<Monthly> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
 
   @override
@@ -28,20 +29,20 @@ class _MonthlyState extends State<Monthly>
 
   @override
   void dispose() {
-    super.dispose();
     _controller.dispose();
+    super.dispose();
   }
 
   Widget buildCard(BuildContext context) {
     DateTime now = DateTime.now();
-    HijriCalendar nowHijri = HijriCalendar.now();
     PrayTime prayerTime = PrayTime(method: widget.method!);
-    List<DataRow> tbl = [];
-    for (int i = 1; i <= 30; i++) {
+    List<Widget> cards = [];
+
+    for (int i = 0; i < 30; i++) {
       var date = now.add(Duration(days: i));
       var hDate = HijriCalendar.fromDate(date);
       DateFormat formatter = DateFormat('MMMM dd, yyyy');
-      String formatted = formatter.format(date);
+      String formattedDate = formatter.format(date);
 
       var times = prayerTime.getPrayerTimes({
         "year": date.year,
@@ -49,98 +50,62 @@ class _MonthlyState extends State<Monthly>
         "mday": date.day,
       }, widget.latitude!, widget.longitude!, parseTimeZoneOffset(now.timeZoneOffset));
 
-
-      Color color = Colors.transparent;
-      if (DateFormat('EEEE').format(date) == 'Sunday') {
-        color = Colors.black12;
-      }
-      if (DateFormat('EEEE').format(date) == 'Friday') {
-        color = Colors.greenAccent;
-      }
-      tbl.add(
-          DataRow(
-            color: MaterialStateColor.resolveWith((states) => color),
-            cells: [
-              DataCell(Text(hDate.toFormat("MMMM dd, yyyy"))),
-              DataCell(Text(formatted)),
-              DataCell(Text(times[0])),
-              DataCell(Text(times[1])),
-              DataCell(Text(times[2])),
-              DataCell(Text(times[3])),
-              DataCell(Text(times[5])),
-              DataCell(Text(times[6])),
-            ],
-          )
+      cards.add(
+        Card(
+          margin: EdgeInsets.all(8.0),
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('${hDate.toFormat("MMMM dd, yyyy")} / $formattedDate', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                SizedBox(height: 8),
+                ...List.generate(times.length, (index) {
+                  var prayer;
+                  switch (index) {
+                    case 0:
+                      prayer = "Fajr";
+                      break;
+                    case 1:
+                      prayer = "Sunrise";
+                      break;
+                    case 2:
+                      prayer = "Dhuhr";
+                      break;
+                    case 3:
+                      prayer = "Asr";
+                      break;
+                    case 4:
+                      prayer = "Maghrib";
+                      break;
+                    case 5:
+                      prayer = "Isha";
+                      break;
+                  }
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 4.0),
+                    child: Text('$prayer : ${times[index]}', style: TextStyle(fontSize: 14)),
+                  );
+                }),
+              ],
+            ),
+          ),
+        ),
       );
     }
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: DataTable(
-              columns: const <DataColumn>[
-                DataColumn(
-                  label: Text(
-                    'Hijri',
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Gregorian',
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Fajr/Sehar Time',
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Sunrise',
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Dhuhr',
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Asr',
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Maghrib/Iftar Time',
-                  ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Isha',
-                  ),
-                ),
-              ],
-              rows: <DataRow>[
-
-                ...tbl,
-              ]),
-        ),
-      ),
+    return SingleChildScrollView(
+      child: Column(children: cards),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-
-    Widget? body = null;
-    if (widget.latitude == null)  {
-      body = Center(child: null,
-      );
-    } else
+    Widget? body;
+    if (widget.latitude == null || widget.longitude == null) {
+      body = Center(child: Text("Please set location"));
+    } else {
       body = buildCard(context);
+    }
     return Scaffold(
       appBar: customAppBar(context, "Monthly View"),
       body: body,
