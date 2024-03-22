07b3adc87d758b2b084f5ebc31e749888b96726b
Muhammad Umer Farooq <umer@lablnet.com>
2024-03-22 10:20:43 +0500

    Update dependencies and upgrade to android v2 embedding.
    Signed-off-by: Muhammad Umer Farooq <umer@lablnet.com>

--- android/app/src/main/kotlin/prayer_time/lablnet/app/prayer_time/MainActivity.kt
+++ android/app/src/main/kotlin/prayer_time/lablnet/app/prayer_time/MainActivity.kt
@@ -1,6 +1,11 @@
 package prayer_time.lablnet.app.prayer_time
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
