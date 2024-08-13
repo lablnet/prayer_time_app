1b1ee5ef010dccd0fdff6cabe8b6e9deea9cbf44
Muhammad Umer Farooq <umer@lablnet.com>
2024-08-13 10:11:45 +0500

    chore: update app version to 1.0.2

--- lib/pages/About.dart
+++ lib/pages/About.dart
@@ -14,7 +14,7 @@ class About extends StatelessWidget {
             ListHeader(title: "App"),
             ListTile(
               title: Text("Prayers Time"),
-              subtitle: Text("Version: 1.0.1"),
+              subtitle: Text("Version: 1.0.2"),
             ),
             ListTile(
               title: Text("Copyright"),
@@ -34,7 +34,7 @@ class About extends StatelessWidget {
             ListHeader(title: "Flutter"),
             ListTile(
               title: Text("Flutter version"),
-              subtitle: Text("3.19.3"),
+              subtitle: Text("3.22.2"),
             ),
             ListTile(
               title: Text("SDK Channel"),
@@ -42,7 +42,7 @@ class About extends StatelessWidget {
             ),
             ListTile(
               title: Text("Dark SDK version"),
-              subtitle: Text("3.3.1"),
+              subtitle: Text("3.4.1"),
             ),
           ],
         ),
